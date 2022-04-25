dc046adf451c19a6d7b89e993ceb63f456ce9811
Jason Hamm <jason.robert.hamm@gmail.com>
2022-04-25 15:07:17 -0400

    Setup carousel movement left and right
    - Create event handlers for `nextBtn` and `prevBtn` to move the carousel
      left and right accordingly
    - Also, update CSS image dimensions
    - Also, move script import from `<head>` with defer to bottom of
      `<body>` because interfering with the `clientWidth`

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("hello world");
+const carouselSlide = document.querySelector(".carousel__slide");
+const carouselImages = document.querySelectorAll(".carousel__img");
+const prevBtn = document.querySelector("#prevBtn");
+const nextBtn = document.querySelector("#nextBtn");
+
+let carouselPosition = 1;
+const carouselWidth = carouselImages[0].clientWidth;
+console.log(carouselWidth);
+
+carouselSlide.style.transform = `translateX(-${
+  carouselWidth * carouselPosition
+}px)`;
+
+nextBtn.addEventListener("click", () => {
+  carouselSlide.style.transition = "transform 0.4s ease-in-out";
+  carouselPosition++;
+  carouselSlide.style.transform = `translateX(-${
+    carouselWidth * carouselPosition
+  }px)`;
+});
+
+prevBtn.addEventListener("click", () => {
+  carouselSlide.style.transition = "transform 0.4s ease-in-out";
+  carouselPosition--;
+  carouselSlide.style.transform = `translateX(-${
+    carouselWidth * carouselPosition
+  }px)`;
+});
