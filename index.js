295e8d076292f354561d74cb554c6256535b64b5
Jason Hamm <jason.robert.hamm@gmail.com>
2022-04-25 15:27:06 -0400

    Setup infinite looping for the carousel
    - Also, guard against carousel breaking when user clicks faster than the
      transition speed. In these cases, just return the `transitionend`
      event handler.

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ carouselSlide.style.transform = `translateX(-${
 }px)`;
 
 nextBtn.addEventListener("click", () => {
+  if (carouselPosition >= carouselImages.length - 1) return;
   carouselSlide.style.transition = "transform 0.4s ease-in-out";
   carouselPosition++;
   carouselSlide.style.transform = `translateX(-${
@@ -20,9 +21,28 @@ nextBtn.addEventListener("click", () => {
 });
 
 prevBtn.addEventListener("click", () => {
+  if (carouselPosition <= 0) return;
   carouselSlide.style.transition = "transform 0.4s ease-in-out";
   carouselPosition--;
   carouselSlide.style.transform = `translateX(-${
     carouselWidth * carouselPosition
   }px)`;
+});
+
+carouselSlide.addEventListener("transitionend", () => {
+  if (carouselImages[carouselPosition].id === "carousel__img--last-dup") {
+    carouselSlide.style.transition = "none";
+    carouselPosition = carouselImages.length - 2;
+    carouselSlide.style.transform = `translateX(-${
+      carouselWidth * carouselPosition
+    }px)`;
+  }
+
+  if (carouselImages[carouselPosition].id === "carousel__img--first-dup") {
+    carouselSlide.style.transition = "none";
+    carouselPosition = carouselImages.length - carouselPosition;
+    carouselSlide.style.transform = `translateX(-${
+      carouselWidth * carouselPosition
+    }px)`;
+  }
 });
